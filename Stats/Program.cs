c65fc2034b0b0ce085a0608a996091cd144c2f6f
Dvoretskyi Oleksandr <nyshtoprivet@gmail.com>
2023-08-13 10:30:37 +0300

    Change for education

--- Stats/Program.cs
+++ Stats/Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Stats;
+using Stats.Loggin;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => options.LoginPath = "/login");
+builder.Services.AddAuthorization();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.UseAuthentication();   // добавление middleware аутентификации 
+//app.UseAuthorization();
+app.UseMiddleware<AuthPathHandler>();
+
+
+app.Map("/", [Authorize] () => $"Hello World!");
 
 app.Run();
+
+//app.MapGet("/", () => "Hello World!");
+
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => options.LoginPath = "/login");
+
